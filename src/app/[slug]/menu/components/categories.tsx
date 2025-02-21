790f0a7728ee14c0445b9eac5278d2b909018520
Jhonathan Amaral <jhon14amaral1@gmail.com>
2025-02-21 20:50:09 -0300

    feat: add restaurant menu page with categories and products

---
+++ src/app/[slug]/menu/components/categories.tsx
@@ -0,0 +1,83 @@
+"use client";
+import {  Prisma } from "@prisma/client";
+import { ClockIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
+import Products from "./products";
+
+interface RestauranteCategoriesProps {
+  restaurant: Prisma.RestaurantGetPayload<{
+    include: {
+      menuCategories: {
+        include: {
+          products: true;
+        };
+      };
+    };
+  }>;
+}
+
+type MenuCategoryWithProducts = Prisma.MenuCategoryGetPayload<{
+  include: {
+    products: true;
+  };
+}>
+
+const RestaurantCategories = ({ restaurant }: RestauranteCategoriesProps) => {
+  const [selectedCategory, setSelectedCategory] = useState<MenuCategoryWithProducts>(restaurant.menuCategories[0]);
+
+  function handleCategoryClick(categorie: MenuCategoryWithProducts) {
+    setSelectedCategory(categorie);
+  }
+
+  const getCategoryButtonVariant = (category: MenuCategoryWithProducts) =>{
+        return selectedCategory.id === category.id ? 'default' : 'secondary'
+  }
+
+  return (
+    <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl  bg-white ">
+      <div className="p-5">
+      <div className="flex items-center gap-3">
+        <Image
+          width={45}
+          height={45}
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+        />
+        <div>
+          <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+          <p className="text-xs opacity-55">{restaurant.description}</p>
+        </div>
+      </div>
+      <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
+        <ClockIcon size={12} />
+        <p>Aberto</p>
+      </div>
+      </div>
+      <ScrollArea className="w-full ">
+            <ScrollBar orientation="horizontal"   />
+            <div className='flex w-max space-x-4 px-3 pb-2 p-0'>
+                {restaurant.menuCategories.map((category) =>(
+                    <Button 
+                    key={category.id} 
+                    variant={getCategoryButtonVariant(category)} 
+                    size='sm' 
+                    className="rounded-full" 
+                    onClick={() => handleCategoryClick(category)}>
+                        {category.name}
+                    </Button>
+                ))}
+               
+            </div>
+      </ScrollArea>
+      <h3 className="px-5 font-semibold pt-6">{selectedCategory.name}</h3>
+  <Products products={selectedCategory.products}/>
+    </div>
+  );
+};
+
+export default RestaurantCategories;
